a1b9cbebe202a2f685942a8e8370fe799e83bce8
Stefano <stefanofabi96@gmail.com>
2023-06-03 22:21:31 -0300

    Crear servidor principal y establecer la conexión

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from 'express'
+import morgan from 'morgan'
+import {Server as SocketServer} from 'socket.io'
+import http from 'http'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+
+var url = ''
+
+// verificar la conexion con mongodb 
+mongoose.Promise = global.Promise
+
+const app = express()
+const PORT = 4000
+
+// Creamos el servidor con el modulo http por defecto en NodeJS
+const server = http.createServer(app)
+const io = new SocketServer(server, {
+    core: {
+        // permitimos peticiones de cualquier origen
+        origin: '*'
+    }
+})
+
+// Middlewares
+app.use(cors())
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Conexion a la base de datos
+mongoose.connect(url, { useNewUrlParser: true}).then(() => {
+    console.log('Conexion a la base de datos establecida')
+
+    // Escucha al puerto 
+    server.listen(PORT, () => {
+        console.log('Servidor ejecutandose en http://localhost:', PORT)
+    })
+}) 
+
+
+
+
+
+
+
