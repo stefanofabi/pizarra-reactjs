2e548db11ee83262f252e363a1141ceda166d79b
Stefano <stefanofabi96@gmail.com>
2023-06-19 03:06:28 -0300

    Verificar campos al guardar mensajes

--- server/routes/message.js
+++ server/routes/message.js
@@ -3,8 +3,18 @@ import controller from '../controllers/message.js'
 
 var router = express.Router()
 
+const checkMessageFields = (req, res, next) => {
+    // Comprueba si las propiedades 'from' y 'message' están presentes y no están vacías
+    if (!req.body.from || !req.body.message) {
+      return res.status(400).json({ error: 'Las propiedades "from" y "message" son requeridas' });
+    }
+    
+    // Aceptar la solicitud
+    next();
+  };
+
 // Definir las rutas de la aplicación
-router.post('/save', controller.save)
+router.post('/save', checkMessageFields, controller.save)
 router.get('/messages', controller.getMessages)
 
 export default router
